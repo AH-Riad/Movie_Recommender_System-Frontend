4a85634d125006ebe9ac2073484d4a0bfc8bf06a
Riad <riadk910@gmail.com>
2026-02-27 14:35:11 +0600

    Using selectbox

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-print("Hiii")
-print("I am developing a ML project named Movie Recommender System")
+import streamlit as st
+import pickle
+
+movies_list = pickle.load(open('movies.pkl', 'rb'))
+movies_list = movies_list["title"].values
+
+st.title("Movie Recommendation System")
+
+option = st.selectbox(
+    'Which movie do you like best?',
+    movies_list)
+'You selected: ', option
