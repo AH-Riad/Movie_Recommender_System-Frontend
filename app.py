c2479a8147816e39660665290ee1c9d3f8180f3d
Riad <riadk910@gmail.com>
2026-02-27 16:09:08 +0600

    Fetching poster using Movie_id

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
+
+def fetch_poster(movie_id):
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=38f54bf2a61923cc247f94ceac5b28f0&language=en-US')
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500" + data['poster_path']
 
 def recommend(movie):
     movie_index = movies[movies["title"] == movie].index[0]
@@ -8,8 +14,14 @@ def recommend(movie):
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
+    recommended_movies_posters= []
+
     for i in movies_list:
+        movie_id = i[0]
         recommended_movies.append((movies.iloc[i[0]].title))
+
+        # fetch poster from api
+        recommended_movies_posters.append(fetch_poster(movie_id))
 
     return recommended_movies
 
@@ -29,6 +41,6 @@ selected_movie_names = st.selectbox(
 'You selected: ', selected_movie_names
 
 if st.button("Recommend"):
-    recommendations = recommend(selected_movie_names)
+    names, posters = recommend(selected_movie_names)
     for i in recommendations:
         st.write(i)
